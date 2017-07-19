b2b6eadeeeb218830901fc9d3ed53c153c369353
uriel621 <uriel621@live.com>
2017-07-18 23:25:40 -0700

    ...

---
+++ React/React Apps/03_projecto_course_sales/src/App.js
@@ -0,0 +1,17 @@
+/*jshint esversion: 6 */ 
+/*jshint ignore:start */
+
+import React from 'react';
+import ReactDom from 'react-dom';
+
+class App extends React.Component {
+    render(){
+        return (
+            <div>
+                <h1>React App 621</h1>
+            </div>
+        )
+    }
+}
+
+ReactDom.render(<App />, document.querySelector('#app'));
