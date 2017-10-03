0611b86e76c3ec1e2fc091778fa0920ec9a22962
uriel621 <uriel621@live.com>
2017-10-02 22:58:03 -0700

    functions

---
+++ Python/02.October/04.Methods_and_Functions/01.methods.py
@@ -0,0 +1,13 @@
+l = [1, 2, 3]
+
+# // like push
+l.append(6)
+
+print(l)
+
+#number of times t appears
+times = l.count(3)
+print(times) 
+
+# print a DOC for help in usage 
+help(l.count)
---
+++ Python/02.October/04.Methods_and_Functions/02.functions.py
@@ -0,0 +1,12 @@
+def print_name(name):
+    print('Hello ' + name)
+
+print_name('Ever')
+
+def addition(num1, num2):
+    """
+    Here is some comments for DOC strings
+    """
+    print (num1 + num2)
+addition(2, 4)
+
