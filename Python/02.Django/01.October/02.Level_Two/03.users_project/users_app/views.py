e8e0b69ad922b6d8c2f424a0a327b0446a42c9ea
uriel621 <uriel621@live.com>
2017-10-29 22:52:24 -0700

    adding users projects

--- Python/02.Django/01.October/02.Level_Two/03.users_project/users_app/views.py
+++ Python/02.Django/01.October/02.Level_Two/03.users_project/users_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from users_app.models import Users
 # Create your views here.
 
 def index(request):
@@ -7,5 +7,6 @@ def index(request):
     return render(request, 'users_app/index.html', context=index)
 
 def user(request):
-    users = {'insert': 'User Table'}
+    users_list = Users.objects.order_by('first_name')
+    users = {'insert': users_list}
     return render(request, 'users_app/views/users.html', context=users)
