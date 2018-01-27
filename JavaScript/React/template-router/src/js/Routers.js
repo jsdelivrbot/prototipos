a71b3a118d932f2c1ed97ffdb2bafe16467631ad
uriel621 <uriel621@live.com>
2018-01-27 01:55:47 -0800

    routes

---
+++ JavaScript/React/template-router/src/js/Routers.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+    // BrowserRouter as Router,
+    HashRouter as Router,    
+    Route,
+    browserHistory,
+    Link
+  } from 'react-router-dom'
+
+import Home from './components/Home';
+import About from './components/About';
+import Topics from './components/Topics';
+
+const Routers = () => (
+    <Router>
+        <div>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/topics">Topics</Link></li>
+            </ul>
+            <hr/>   
+            <Route exact path="/" component={ Home }/>
+            <Route path="/about" component={ About }/>
+            <Route path="/topics" component={ Topics }/>
+        </div>
+    </Router>
+)
+
+export default Routers;
--- JavaScript/React/template-router/webpack.config.js
+++ JavaScript/React/template-router/webpack.config.js
@@ -1,25 +1,32 @@
 var webpack = require("webpack");
+var path = require('path');
 
 module.exports = {
-	entry: {
-		app: "./src/index.js"
-	},
-	output: {
-		filename:"build/bundle.js",
-        sourceMapFilename: "build/bundle.map"
-	},
-	devtool: '#source-map',
+	entry: './src/js/index.js',
+  	output: {
+      path: path.resolve(__dirname, 'public'),
+      filename: 'bundle.js',
+			publicPath: '/public'
+		},
 
-	module: {
-		loaders: [
-			{
-				test: /\.jsx?$/,
-				exclude: /(node_modules|bower_components)/,
-				loader: 'babel-loader',
-				query:{
-					presets:['react', 'es2015']
+		module: {
+			loaders: [
+				{
+			  	test: /\.jsx?$/,
+			  	exclude: /(node_modules)/,
+			  	loader: 'babel-loader',
+					query: {
+						presets: ['react', 'es2015']
+					}
+				},
+				{
+			  	test: /\.css$/,
+			  	loaders: ['style-loader', 'css-loader']
+				},
+				{
+			  	test: /\.(jpe?g|png|gif|svg)$/i,
+			  	loaders: ['file-loader', 'image-webpack-loader']
 				}
-			}
-		]
-	}
+			]
+		},
 }
